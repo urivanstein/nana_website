b18abcd13e25cf991edda1c8d0e95986ab29a9f3
urivanstein <urivanstein@gmail.com>
2016-01-17 19:30:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Natasja personal trainer</title>
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<!--navigation bar-->
+<div id="navigation">
+    <ul>
+        <li><a href="index.php#home">Home</a></li>
+        <li><a href="index.php#about">About Us</a></li>
+        <li><a href="index.php#pictures">Pictures</a></li>
+        <li><a href="index.php#contact">Contact</a></li>
+    </ul>
+</div>
+
+<!--home-->
+<div id="home" class="wrapper">
+    <img id="siteimg" src="images/pose.jpg">
+</div>
+
+<!--about-->
+<div id="about" class="wrapper">
+    <aside id="first_aside">
+        <img id="headshot" src="images/natasja.jpg">
+    </aside>
+    <article id="first_article">
+        <div id="container">
+            <h1>About Us</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Aspernatur corporis cumque hic iste neque odit omnis, optio quam quia sed.
+            Alias consequuntur expedita nisi placeat
+            praesentium quam quos, repellat voluptatibus!
+            </p>
+        </div>
+    </article>
+</div>
+
+<!--pricing-->
+<div id="pictures" class="wrapper">
+    
+</div>
+
+<!--contact-->
+<div id="contact" class="wrapper">
+    <img id="siteimg" src="images/sweaty_belly.jpg"
+</div>
+
+<script src="main.js"></script>
+</body>
+</html>    
---
+++ main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by User on 1/6/2016.
+ */
+
+
+
